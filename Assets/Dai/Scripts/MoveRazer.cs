422b1414f0774708222c3ac0ece69141993bf323
OP16Dai <puccho318318@gmail.com>
2017-11-06 15:52:08 +0900

    StartDai 11/06/15:51

---
+++ Assets/Dai/Scripts/MoveRazer.cs
@@ -0,0 +1,68 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MoveRazer : MonoBehaviour {
+
+    //プレイヤ―の位置取得用
+    public GameObject Player;
+    
+
+    //出現位置の設定(レーザー)
+    public float RazerX;
+    public float RazerY;
+    public float RazerZ;
+
+    //移動速度
+    public float Speed;
+
+    //移動距離保管
+    float x_move;
+    float y_move;
+    float z_move;
+
+
+
+    // Use this for initialization
+    void Start () {
+        //速度設定
+        SetSpeed();
+        //初期位置の設定
+        transform.position = new Vector3(RazerX, RazerY, RazerZ);
+
+    }
+
+    // Update is called once per frame
+    void Update () {
+        //移動処理
+        MoveTo();
+        //レーザー削除処理
+        RemoveRazer();
+    }
+
+
+    //速度設定
+    void SetSpeed()
+    {
+        //速度と距離から毎フレームの移動距離を測定
+        x_move = (0 - RazerX) / (Speed * 60);
+        y_move = (0 - RazerY) / (Speed * 60);
+        z_move = (0 - RazerZ) / (Speed * 60);
+    }
+
+    //移動処理
+    void MoveTo()
+    {
+        //1フレームごとに位置を変更
+        transform.position = new Vector3(transform.position.x + x_move, transform.position.y + y_move, transform.position.z + z_move);
+    }
+
+    //プレイヤーを通り越したら削除
+    void RemoveRazer()
+    {
+        if (transform.position.z < Player.transform.position.z)
+        {
+            Destroy(gameObject);
+        }
+    }
+}
