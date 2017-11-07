1bea2408f8699e5e17f3d52d58f85c9310183c9f
ico <travelerofwind29@gmail.com>
2017-11-08 03:50:59 +0900

    Takayan 2017/11/08 03:50 PlayerChara追加

---
+++ Assets/Takayan/Script/PlayerMove.cs
@@ -0,0 +1,34 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerMove : MonoBehaviour {
+
+
+    //Animatorコンポーネント
+    Animator animator;
+
+    //設定したフラグ名
+    const string key_isJump = "isJump";
+
+
+    void Start () {
+        //自分に設定されているAnimatorコンポーネントを取得する
+        this.animator = GetComponent<Animator>();
+    }
+	
+	void Update () {
+
+        //上ボタンを押しているかどうか
+        if (Input.GetKey(KeyCode.UpArrow))
+        {
+            //WaitからJumpに遷移する
+            this.animator.SetBool(key_isJump, true);
+        }
+        else
+        {
+            //RunからWaitに移動する
+            this.animator.SetBool(key_isJump, false);
+        }
+    }
+}
