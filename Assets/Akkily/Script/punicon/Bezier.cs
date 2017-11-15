3d1024cfffad11d0d53dfa8bcf27d51a4831fc5b
Akkily <Akkily0034@users.noreply.github.com>
2017-11-15 13:45:19 +0900

    Akkily 2017/11/15 13:44 punicon試作

---
+++ Assets/Akkily/Script/punicon/Bezier.cs
@@ -0,0 +1,88 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Bezier
+{
+  #region プロパティ
+
+  /// <summary>
+  /// 始点
+  /// </summary>
+  public Vector2 P1
+  {
+    get;
+    set;
+  }
+
+  /// <summary>
+  /// 始点制御点
+  /// </summary>
+  public Vector2 P2
+  {
+    get;
+    set;
+  }
+
+  /// <summary>
+  /// 終点制御点
+  /// </summary>
+  public Vector2 P3
+  {
+    get;
+    set;
+  }
+
+  /// <summary>
+  /// 終点
+  /// </summary>
+  public Vector2 P4
+  {
+    get;
+    set;
+  }
+
+  /// <summary>
+  /// パラメータが有効かどうか取得します。
+  /// </summary>
+  public bool IsValid
+  {
+    get
+    {
+      if( P1 == null ) return false;
+      if( P2 == null ) return false;
+      if( P3 == null ) return false;
+      if( P4 == null ) return false;
+      return true;
+    }
+  }
+
+  #endregion
+  
+  #region メソッド
+
+  /// <summary>
+  /// パラメータt(0～1)を指定して位置を取得します。
+  /// </summary>
+  /// <param name="t"></param>
+  /// <returns></returns>
+  public Vector2 GetPosition( float t )
+  {
+    if( t <= 0 ) return new Vector2(P1.x, P1.y);
+    if( t >= 1 ) return new Vector2(P2.x, P2.y);
+
+    var x1 = P1.x;
+    var x2 = P2.x;
+    var x3 = P3.x;
+    var x4 = P4.x;
+    var y1 = P1.y;
+    var y2 = P2.y;
+    var y3 = P3.y;
+    var y4 = P4.y;
+    var tp = 1 - t;
+    var x = t * t * t * x4 + 3 * t * t * tp * x3 + 3 * t * tp * tp * x2 + tp * tp * tp * x1;
+    var y = t * t * t * y4 + 3 * t * t * tp * y3 + 3 * t * tp * tp * y2 + tp * tp * tp * y1;
+    return new Vector2(x, y);
+  }
+
+  #endregion
+}
---
+++ Assets/Akkily/Script/punicon/PunipuniMesh.cs
@@ -0,0 +1,144 @@
+﻿using UnityEngine;
+using System.Collections;
+
+/// <summary>
+/// ぷにぷにコントローラー用メッシュを保持します。
+/// </summary>
+public class PunipuniMesh : MonoBehaviour
+{
+  #region フィールド(private)
+
+  /// <summary>
+  /// メッシュ。
+  /// </summary>
+  Mesh _mesh;
+
+  /// <summary>
+  /// オリジナル頂点座標配列(中心以外)
+  /// </summary>
+  Vector3[] _org_vertexes;
+
+  #endregion
+
+  #region プロパティ
+
+  /// <summary>
+  /// 
+  /// </summary>
+  public Mesh Mesh
+  {
+    get { return _mesh; }
+  }
+
+  /// <summary>
+  /// 
+  /// </summary>
+  public Vector3[] Vertexes
+  {
+    get
+    {
+      Vector3[] vtx = new Vector3[_mesh.vertices.Length - 1];
+      for( int n = 0; n < vtx.Length; n++ ) {
+        vtx[n] = _mesh.vertices[n];
+      }
+      return vtx;
+    }
+    set 
+    {
+      var vtx = _mesh.vertices;
+      for( int n = 0; n < value.Length; n++ ) {
+        vtx[n] = value[n];
+      }
+      _mesh.vertices = vtx;
+      _mesh.RecalculateBounds();
+    }
+  }
+
+  /// <summary>
+  /// 
+  /// </summary>
+  public Vector3 CenterPoint
+  {
+    get
+    {
+      return _mesh.vertices[_mesh.vertices.Length - 1];
+    }
+    set
+    {
+      var vtx = _mesh.vertices;
+      vtx[vtx.Length - 1] = value;
+      _mesh.vertices = vtx;
+      _mesh.RecalculateBounds();
+    }
+  }
+
+  /// <summary>
+  /// 
+  /// </summary>
+  public Vector3[] OriginalVertexes
+  {
+    get { return _org_vertexes; }
+  }
+
+  #endregion
+
+  /// <summary>
+  /// 
+  /// </summary>
+  /// <param name="vertexCount"></param>
+  /// <param name="radius"></param>
+  public PunipuniMesh( int vertexCount, float radius )
+  {
+    CreateMesh(vertexCount, radius);
+  }
+
+  /// <summary>
+  /// ぷにぷにコントローラー用メッシュを生成します。
+  /// </summary>
+  /// <param name="vertexCount"></param>
+  /// <param name="radius"></param>
+  /// <returns></returns>
+  bool CreateMesh( int vertexCount, float radius )
+  {
+    _mesh = new Mesh();
+
+    // 頂点の生成
+    Vector3[] points = new Vector3[vertexCount + 1];
+    var angle = System.Math.PI * 2;
+    for( int n = 0; n < vertexCount; n++ ) {
+      var r = angle / vertexCount * n;
+      var x = System.Math.Cos(r) * radius;
+      var y = System.Math.Sin(r) * radius;
+      points[n] = new Vector3((float)x, (float)y, 0);
+    }
+    points[vertexCount] = new Vector3(0, 0, 0);   // 中心
+    _mesh.vertices = points;
+
+    _org_vertexes = new Vector3[points.Length - 1];
+    for( int n = 0; n < points.Length - 1; n++ ) {
+      _org_vertexes[n] = new Vector3( points[n].x, points[n].y, points[n].z );
+    }
+
+    // 頂点インデックス生成
+    int[] indexes = new int[vertexCount * 3];
+    for( int n = 0; n < vertexCount; n++ ) {
+      indexes[n * 3 + 0] = n;
+      indexes[n * 3 + 1] = (n + 1) % vertexCount;
+      indexes[n * 3 + 2] = vertexCount;
+    }
+    _mesh.triangles = indexes;
+
+    // 頂点色の生成
+    Color[] colors = new Color[vertexCount + 1];
+    for( int n = 0; n < vertexCount; n++ ) {
+      colors[n] = new Color(1, 1, 1, 1);
+    }
+    colors[vertexCount] = new Color(0, 0, 0, 0.0f);
+    _mesh.colors = colors;
+
+    //_mesh.RecalculateNormals();
+    _mesh.RecalculateBounds();
+
+    return true;
+  }
+}
