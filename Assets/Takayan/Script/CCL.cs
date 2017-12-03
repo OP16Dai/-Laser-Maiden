e90812bc6b30eadecbe7705d02da78b2552e9126
ico <travelerofwind29@gmail.com>
2017-12-04 03:21:01 +0900

    Takayan 2017/12/04 3:20 タッチ操作追加

--- Assets/Takayan/Script/CCL.cs
+++ Assets/Takayan/Script/CCL.cs
@@ -10,7 +10,44 @@ public class CCL : MonoBehaviour {
     private Vector3 offset2;
 
     Animator anim;
-                                    // Use this for initialization
+    // Use this for initialization
+    [SerializeField]
+    public float x_aspect = 16.0f;
+    [SerializeField]
+    public float y_aspect = 9.0f;
+
+    void Awake()
+    {
+        Camera camera = GetComponent<Camera>();
+        Rect rect = calcAspect(x_aspect, y_aspect);
+        camera.rect = rect;
+    }
+
+    private Rect calcAspect(float width, float height)
+    {
+        float target_aspect = width / height;
+        float window_aspect = (float)Screen.width / (float)Screen.height;
+        float scale_height = window_aspect / target_aspect;
+        Rect rect = new Rect(0.0f, 0.0f, 1.0f, 1.0f);
+
+        if (1.0f > scale_height)
+        {
+            rect.x = 0;
+            rect.y = (1.0f - scale_height) / 2.0f;
+            rect.width = 1.0f;
+            rect.height = scale_height;
+        }
+        else
+        {
+            float scale_width = 1.0f / scale_height;
+            rect.x = (1.0f - scale_width) / 2.0f;
+            rect.y = 0.0f;
+            rect.width = scale_width;
+            rect.height = 1.0f;
+        }
+        return rect;
+    }
+
     void Start () {
         player = GameObject.FindGameObjectWithTag("Player");
         //プレイヤーとカメラ間の距離を取得してそのオフセット値を計算し、格納します。
@@ -23,7 +60,9 @@ public class CCL : MonoBehaviour {
         //プレイヤーのアニメーターを取得
         anim = GameObject.FindGameObjectWithTag("Player").GetComponent<Animator>();
 
-        
+        Awake();
+
+
     }
 	
 	// Update is called once per frame
@@ -46,24 +85,6 @@ public class CCL : MonoBehaviour {
         //カメラの transform 位置をプレイヤーのものと等しく設定します。ただし、計算されたオフセット距離によるずれも加えます。
         transform.position = player.transform.position + offset;
 
-        /*
-        if (this.anim.GetCurrentAnimatorStateInfo(0).IsTag("Jump") == true)
-        {
-
-            //カメラの transform 位置をプレイヤーのものと等しく設定します。ただし、計算されたオフセット距離によるずれも加えます。
-            transform.position = player.transform.position + offset;
-
-           
-
-        }
-        else
-        {
-            offset2.y -= (player.transform.position.y);
-            transform.position = player.transform.position + offset2;
-            offset2.y += (player.transform.position.y);
-
-  
-        }
-        */
+      
     }
 }
