564f17f4f25aea83d945d8506c95c7efe67430d8
Gatalga <gatalgaonline@gmail.com>
2018-01-23 15:38:52 +0900

    Fujii 1/23 15:38 Home・Title・StageSelectScene追加

---
+++ Assets/Fujii/ButtonScript.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class ButtonScript : MonoBehaviour {
+
+    [SerializeField]
+    private string loadName;
+
+    public void OnClick()
+    {
+        SceneManager.LoadScene(loadName);
+        //Debug.Log("Button click");
+    }
+
+    // Use this for initialization
+    void Start () {
+        
+    }
+
+    // Update is called once per frame
+    void Update (){
+        
+    }
+}
---
+++ Assets/Fujii/TitleScene/TextBlinkScript.cs
@@ -0,0 +1,38 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class TextBlinkScript : MonoBehaviour {
+
+    public Text Qtext;
+    float a_color;
+    bool flag_G;
+
+    // Use this for initialization
+    void Start () {
+        a_color = 0;
+    }
+	
+	// Update is called once per frame
+	void Update () {
+
+        // Change the transparency of text
+        Qtext.color = new Color(50, 50, 50, a_color);
+        if (flag_G)
+            a_color -= Time.deltaTime;
+        else
+            a_color += Time.deltaTime;
+        if (a_color < 0)
+        {
+            a_color = 0;
+            flag_G = false;
+        }
+        else if (a_color > 1)
+        {
+            a_color = 1;
+            flag_G = true;
+        }
+
+    }
+}
